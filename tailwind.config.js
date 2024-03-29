ef4de648b744df34d26f5e3ab7ba35b17e24d501
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2024-03-29 23:25:26 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        work: [
+          '"Work Sans"'
+        ]
+      }, 
+      screens: {
+         'desktop': '1300px',
+      },
+      colors: {
+        batao: {
+          bg: '#F9FCFF',
+          primary: '#353565',
+          text: '#6C757D',
+          card: '#FFFFFF',
+          shadow: '#90B8E840',
+          green: '#0E560E',
+          disabled: '#CECECE',
+          'sub-text': '#474747',
+          label: '#808080',
+          accent: '#ED1A53',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
